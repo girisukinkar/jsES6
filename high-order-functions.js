374ed90a35bbd497fa3a9c595f569d3cf265b03c
girisukinkar <girisukinkar@gmail.com>
2020-12-08 10:55:49 +0530

    Higher order functions

--- high-order-functions.js
+++ high-order-functions.js
@@ -1,18 +1,28 @@
+//SOME AND EVERY : both functions return boolean
 const users = [{
     id: 1,
-    name : 'Girish'
+    name : 'Girish',
+    complete:true
 },{
     id:2,
-    name : 'Devil'
-}];
+    name : 'Devil',
+    complete:true
+}
+,{
+    id:3,
+    name : 'Dexter',
+    complete:true
+}
+];
 
 
 const found = users.some(user => user.id === 2);
 console.log(`User id exists : ${found}`);
+
+const allRegistered = users.every(user => user.complete === true);
+console.log(`Every user is registered: ${allRegistered}`);
+
 if(found){
-    const user = users.filter(user => user.id === 2);
-    console.log('this is user', user[0]);
+    const user = users.filter(user => user.id === 2  );
     const {id: id , name: name} = user[0];
-    console.log(name);
 }
-console.table(users);
