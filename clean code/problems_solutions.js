9d17e27d8d70c518ab1dd6b5802332656432a510
girisukinkar <girisukinkar@gmail.com>
2020-12-14 13:22:16 +0530

    Coding principles

---
+++ clean code/problems_solutions.js
@@ -0,0 +1,37 @@
+
+/* ###############################
+FUNCTIONS SHOULD ONLY DO ONE THING AND DO IT WELL
+/* ############################### */
+
+/*
+Lets try with an example. Assume we have a list of grades from a classroom. Our classroom has 5 girls, 5 boys and each of them has a grade between 0 and 20.
+*/
+
+let grades = [
+    {name: 'John', grade: 8, sex: 'M'},
+    {name: 'Sarah', grade: 12, sex: 'F'},
+    {name: 'Bob', grade: 16, sex: 'M'},
+    {name: 'Johnny', grade: 2, sex: 'M'},
+    {name: 'Ethan', grade: 4, sex: 'M'},
+    {name: 'Paula', grade: 18, sex: 'F'},
+    {name: 'Donald', grade: 5, sex: 'M'},
+    {name: 'Jennifer', grade: 13, sex: 'F'},
+    {name: 'Courtney', grade: 15, sex: 'F'},
+    {name: 'Jane', grade: 9, sex: 'F'}
+]
+
+/*
+I want to know a few things about this:
+
+The average grade of this classroom
+The average grade of the boys
+The average grade of the girls
+The higher note among the boys
+The higher note among the girls
+*/
+
+
+
+
+
+/* FUNCTION Question ENDS */
