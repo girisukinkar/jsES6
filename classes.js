cc67f5a3138e2c5749389696083bd7353e3df618
girisukinkar <girisukinkar@gmail.com>
2021-01-17 12:26:01 +0530

    ES6 way of writing classes and accessing the properties / methods

--- classes.js
+++ classes.js
@@ -1,26 +1,42 @@
 class Human {
-    constructor(){
-        this.gender = 'male';
-    }
-    printGender(){
+    gender = "male";
+    printGender = () => {
         console.log(this.gender);
     }
 }
-
-class Person extends Human { 
+//OLD type
+/* class Person extends Human { 
 
     constructor(){
         super();
         this.name  = "Girish";
-        
+        this.age =  27
     }
 
     printName(){
         console.log(`Person ${this.name} gender is ${this.gender}`);
         console.log(this.name);
     }
+    printAge = () => {
+        console.log(this.age);
+    }
+} */
+
+//New ES6 way to do it : No constructor no super()
+class Person extends Human { 
+     name  = "Girish";
+     age =  27
+
+    printName = () => {
+        console.log(`Person ${this.name} gender is ${this.gender}`);
+        console.log(this.name);
+    }
+    printAge = () => {
+        console.log(this.age);
+    }
 }
 
 const person = new Person();
 person.printGender();
 person.printName();
+person.printAge();
