f6042782c4090767bacb4415dc07fa9de7d441b6
girisukinkar <girisukinkar@gmail.com>
2021-04-25 17:11:46 +0530

    added event delegation example and this definition

--- event-delegation.js
+++ event-delegation.js
@@ -1,6 +1,7 @@
 // This concept suggests . if there is a parent with numerous amount of childs which has a click functionality
 // Then instead of attaching a click handler to every child we must attach only one to the parent and make use of e.target to perform operations
 
+//DEFINITIOn : The idea is that if we have a lot of elements handled in a similar way, then instead of assigning a handler to each of them – we put a single handler on their common ancestor.
 let selectedID;
 document.getElementById('a').addEventListener('click',(e) =>{
 
--- whatIsThis.js
+++ whatIsThis.js
@@ -1,1 +1,2 @@
 //https://blog.bitsrc.io/what-is-this-in-javascript-3b03480514a7
+//https://www.w3schools.com/js/js_this.asp
