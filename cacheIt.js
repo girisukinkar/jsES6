2b31efaaa843e2e2911fe0bb0e8ac59f2bd3fc5e
gsukinkar <girisukinkar@gmail.com>
2024-12-15 16:05:14 +0530

    Using Closure to cache results for performance optimization

---
+++ cacheIt.js
@@ -0,0 +1,37 @@
+// Using closures we can cach any operation to improve performance
+
+
+function createFetchSuggestios () {
+    const cache = {}
+
+    return async function fetchSuggestion(query) {
+        console.log(query, 'query')
+        if (!query) return
+        if (query in cache) {
+            console.log('Fetching items from cacheee')
+            return cache[query];
+        }
+
+        console.log('Fetching Records')
+        const response = await fetch('json/real_users_list.json')
+        const res = await response.json()
+        // console.log(res)
+
+        const lowerCasedSearch = query.toLowerCase();
+
+        cache[query] = res.filter(user =>
+            user.firstName.toLowerCase().startsWith(lowerCasedSearch) ||
+            user.lastName.toLowerCase().startsWith(lowerCasedSearch)
+          ).map(user => `${user.firstName} ${user.lastName}`);
+    
+        return cache[query]
+    }
+}
+
+const search = createFetchSuggestios()
+
+async function run (name) {
+    console.log(name.value)
+  const data = await search(name.value)
+  console.log(data)
+}
